9f95d4a97fb65dfebff7a97c5f22d930c4634f83
mrlucca <vini.lucca42@gmail.com>
2021-02-19 09:56:58 -0300

    Optimized performance (1)

--- main.go
+++ main.go
@@ -2,23 +2,14 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/mrlucca/swapi_consumer/src"
 )
 
 func main() {
-	// spyderConfig := config.GetConfig()
-	// fmt.Println(spyderConfig)
-	// spyderConfig.SetMaxGoroutines(10)
-	// fmt.Println(spyderConfig)
-	getPeople := src.GetPeople()
-	for i := 1; i < 50; i++ {
-		peopleReference := strconv.Itoa(i)
-		people, err := getPeople(peopleReference)
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Println(people)
+	done := make(chan bool, 1)
+	go src.GetPeople(done)
+	if <-done {
+		fmt.Println("close")
 	}
 }
--- src/getPeople.go
+++ src/getPeople.go
@@ -6,39 +6,52 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/mrlucca/swapi_consumer/config"
 	"github.com/mrlucca/swapi_consumer/models"
 )
 
-func GetPeople() func(peopleReference string) (models.People, error) {
-	log.Println("Init get people flow")
-	err := verifyConnectionInUrlBase()
+func getData(id string, jobs <-chan int, rawData chan<- []byte) {
+	url := fmt.Sprintf("%v/people/%v", config.URLS["BASE"], id)
+	log.Println("Get people -> " + url)
+	response, err := http.Get(url)
+	if err != nil || response.StatusCode != 200 {
+		msg_err := fmt.Sprintf("Not possible get page, status code = %v", response.StatusCode)
+		log.Fatalln(msg_err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	rawData <- body
+
+}
+
+func formatData(body []byte) {
+	log.Println("Trasnform people data in string")
+	var people models.People
+	err := json.Unmarshal(body, &people)
 	if err != nil {
-		panic(err.Error())
+		log.Fatalln("Object is not found")
 	}
-	url_base := fmt.Sprintf("%v/people/", config.URLS["BASE"])
-	return func(peopleReference string) (models.People, error) {
-		url := url_base + peopleReference
-		log.Println("Get people -> " + url)
-		response, err := http.Get(url)
-		if err != nil {
-			msg_err := fmt.Sprintf("Not possible get page, status code = %v", response.StatusCode)
-			log.Fatalln(msg_err)
-		}
-		log.Println("Trasnform people data in string")
-		body, err := ioutil.ReadAll(response.Body)
-		defer response.Body.Close()
-		if err != nil {
-			return models.People{}, fmt.Errorf("No users in stream of bytes")
-		}
-		var people models.People
-		err = json.Unmarshal(body, &people)
-		if err != nil {
-			log.Fatalln("Object is not found")
-		}
+	fmt.Println(people)
+}
 
-		return people, nil
-
+func GetPeople(done chan<- bool) {
+	const numOfGet int = 80
+	jobs := make(chan int, numOfGet)
+	rawData := make(chan []byte, numOfGet)
+	for i := 1; i <= numOfGet; i++ {
+		id := strconv.Itoa(i)
+		go getData(id, jobs, rawData)
 	}
+	for i := 1; i <= numOfGet; i++ {
+		jobs <- i
+	}
+	for i := 1; i <= numOfGet; i++ {
+		response := <-rawData
+		formatData(response)
+	}
+	close(jobs)
+	done <- true
+
 }
